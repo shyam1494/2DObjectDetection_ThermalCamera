e3ab8a4f0e6bba41e4ff92ae15e06728b4a9e514
SudharsanamShyamsundar <44057637+SudharsanamShyamsundar@users.noreply.github.com>
2022-05-13 18:34:44 +0100

    changes in outputToTarget method
    Copied the memory from GPU to CPU

--- utils/plots.py
+++ utils/plots.py
@@ -94,7 +94,7 @@ def output_to_target(output, width, height):
     targets = []
     for i, o in enumerate(output):
         if o is not None:
-            for pred in o:
+            for pred in o.cpu().numpy():
                 box = pred[:4]
                 w = (box[2] - box[0]) / width
                 h = (box[3] - box[1]) / height
